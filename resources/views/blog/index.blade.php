db8618b13b43c506f710f6e3e8d27233bec4b2ee
Tianyun Zhang <doowzs@qq.com>
2019-03-28 00:27:45 +0800

    show published time for posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,7 +2,10 @@
 @section('content')
     <p>总共有{{ $posts->total() }}篇文章。</p>
     @foreach($posts as $post)
-        <a href="{{ route('blog.view', ['slug' => $post->slug]) }}">{{$post->title}}</a>
+        <p>
+            <a href="{{ route('blog.view', ['slug' => $post->slug]) }}">{{ $post->title }}</a>
+            <small>发布于 {{ $post->published_at }}</small>
+        </p>
     @endforeach
     {!! $posts->render() !!}
 @stop
