80f275564dc6a9e790f9e95a0f8993ea458b45df
Tianyun Zhang <doowzs@qq.com>
2019-03-29 21:11:38 +0800

    update deploy script

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+unset GIT_DIR
+
 echo "[AUTO] Start auto deployment."
-git pull
+cd /PATH_TO_PROJECT/
+sudo -u WEB_USER_NAME /PATH_TO_GIT/git pull
 
 echo "[AUTO] Pull from remote repo succeed!"
-php artisan migrate --force
-php artisan vireo:cache
+sudo -u WEB_USER_NAME /PATH_TO_PHP/php artisan vireo:cache
 
 echo "[AUTO] Auto deployment succeed!"
